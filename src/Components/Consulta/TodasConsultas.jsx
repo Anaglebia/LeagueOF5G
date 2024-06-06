e214e3280c5b2cad71c60259e978b9bf5da4ba5c
Maytê <maytearaujo@yahoo.com.br>
2024-06-06 17:35:41 -0300

    feat: atualização todas as consultas

--- src/Components/Consulta/TodasConsultas.jsx
+++ src/Components/Consulta/TodasConsultas.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
 const TodasConsultas = () => {
   const [consultas, setConsultas] = useState([]);
 
@@ -19,16 +18,32 @@ const TodasConsultas = () => {
 
   return (
     <div>
-      <h2>TodasConsultas</h2>
+      <h2>Relatório de Consultas</h2>
+
       {consultas.length > 0 ? (
         consultas.map((consulta) => (
           <div key={consulta.id}>
-            <p>Data: {consulta.data}</p>
+            <table>
+              <tr>
+                <td>Data: {consulta.data}</td>
+                <td>Horário: {consulta.horario}</td>
+              </tr>
+              <tr>
+                <td>Especialidade: {consulta.especialidade}</td>
+              </tr>
+              <tr>
+                <td>Nome do Médico: {consulta["nome-medico"]}</td>
+              </tr>
+              <tr>
+                <td>Observação: {consulta.observacao}</td>
+              </tr>
+            </table>
+            {/* <p>Data: {consulta.data}</p>
             <p>Horário: {consulta.horario}</p>
             <p>Especialidade: {consulta.especialidade}</p>
             <p>Nome do Médico: {consulta["nome-medico"]}</p>
             <p>Observação: {consulta.observacao}</p>
-            <hr />
+            <hr /> */}
           </div>
         ))
       ) : (
